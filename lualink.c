900c3f96dd47b9b0687fbd6ae3edd298e914a712
Thomas Wright <vorduga@gmail.com>
2012-10-20 00:55:11 -0400

    Changed some library stuff around to make this compile more easily.
    Now it should be fairly straightforward to compile on ubuntu.

--- lualink.c
+++ lualink.c
@@ -1,6 +1,6 @@
-#include <lua.h>
-#include <lualib.h>
-#include <lauxlib.h>
+#include <lua5.2/lua.h>
+#include <lua5.2/lualib.h>
+#include <lua5.2/lauxlib.h>
 #include <GL3/gl3w.h>
 #include <GL/glfw.h>
 
